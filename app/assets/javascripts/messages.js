35e634af35bbe72a768949adab9f0e7ad11a839b
ramuneru <accuracy.precision.continuum@gmail.com>
2019-09-07 19:01:20 +0900

    Modified-ajax-message-function
    メッセージ送信機能の非同期通信
    messages.js編集
    SCSSー画像サイズー微調整
    jbuilder作成

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,51 +1,50 @@
 $(function(){
+  function buildHTML(message){
+    let MessageImage = (message.image) ? `<img src='${ message.image }', class='lower_message__image' >` : ""
+    console.log(MessageImage);
+    let html = `<div class='message'>
+                  <div class='message__upper-info'>
+                    <div class='message__upper-info__talker'>
+                      ${ message.user_name }
+                    </div>
+                    <div class='message__upper-info__date'>
+                      ${ message.date }
+                    </div>
+                  </div>
+                  <div class='lower_message'>
+                    <p class='message__text'>
+                      ${ message.text }
+                    </p>
+                      ${ MessageImage }
+                </div>`
+    return html;
+  }
 
-  $('#message_content').on('submit', function(e) {
+  $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    // let formData = new FormData(this);
-    console.log("ok");
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      let html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val('');
+      $('#message_image').val('');
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      return false
+    })
+    .fail(function() {
+      alert('入力エラー：文章または画像を入力してください');
+    })
+    .always(function() {
+      $('.submit-btn').attr('disabled', false);
+    });
   });
 });
-
-// $('#the-form').submit(function(event) {
-//   // HTMLでの送信をキャンセル
-//   event.preventDefault();
-
-//   // 操作対象のフォーム要素を取得
-//   var $form = $(this);
-
-//   // 送信ボタンを取得
-//   var $button = $form.find('button');
-
-//   // 送信
-//   $.ajax({
-//       url: $form.attr('action'),
-//       type: $form.attr('method'),
-//       data: $form.serialize(),
-//       timeout: 10000,  // 単位はミリ秒
-
-//       // 送信前
-//       beforeSend: function(xhr, settings) {
-//           // ボタンを無効化し、二重送信を防止
-//           $button.attr('disabled', true);
-//       },
-//       // 応答後
-//       complete: function(xhr, textStatus) {
-//           // ボタンを有効化し、再送信を許可
-//           $button.attr('disabled', false);
-//       },
-
-//       // 通信成功時の処理
-//       success: function(result, textStatus, xhr) {
-//           // 入力値を初期化
-//           $form[0].reset();
-           
-//           alert('OK');
-//       },
-
-//       // 通信失敗時の処理
-//       error: function(xhr, textStatus, error) {
-//           alert('NG...');
-//       }
-//   });
-// });
