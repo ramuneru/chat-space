544fe91d825ee204a21b4022d5f984ef596fc6b2
ramuneru <accuracy.precision.continuum@gmail.com>
2019-09-12 13:25:18 +0900

    Update application.rb
    timezone修正

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module ChatSpace
       g.test_framework false
       config.i18n.default_locale = :ja
     end
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
